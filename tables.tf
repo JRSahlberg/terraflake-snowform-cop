71d239b857d2b2e31d46bac75aa7f8b505e75107
Jean Rafael Sahlberg <jrsahlberg@gmail.com>
2023-08-07 23:34:33 -0300

    remove comments

--- tables.tf
+++ tables.tf
@@ -3,41 +3,35 @@ resource "snowflake_table" "user_profiles" {
   database = snowflake_database.db.name
   schema   = snowflake_schema.test_schema.name
   name     = "USER_PROFILES"
-  comment  = "Table to store user profile details."
 
   column {
     name     = "USER_ID"
-    type     = "INTEGER"
+    type     = "NUMBER(38,0)"
     nullable = false
-    comment  = "Unique identifier for a user."
   }
 
   column {
     name     = "EMAIL"
     type     = "VARCHAR(255)"
     nullable = false
-    comment  = "Email address of the user."
   }
 
   column {
     name     = "FIRST_NAME"
     type     = "VARCHAR(100)"
     nullable = true
-    comment  = "First name of the user."
   }
 
   column {
     name     = "LAST_NAME"
     type     = "VARCHAR(100)"
     nullable = true
-    comment  = "Last name of the user."
   }
 
   column {
     name     = "DATE_JOINED"
     type     = "DATE"
     nullable = true
-    comment  = "The date when the user joined."
   }
 }
 
@@ -47,40 +41,35 @@ resource "snowflake_table" "orders" {
   database = snowflake_database.db.name
   schema   = snowflake_schema.test_schema.name
   name     = "ORDERS"
-  comment  = "Table to store orders placed by users."
 
   column {
     name     = "ORDER_ID"
-    type     = "INTEGER"
+    type     = "NUMBER(38,0)"
     nullable = false
     comment  = "Unique identifier for an order."
   }
 
   column {
     name     = "USER_ID"
-    type     = "INTEGER"
+    type     = "NUMBER(38,0)"
     nullable = false
-    comment  = "Foreign key referencing USER_PROFILES table."
   }
 
   column {
     name     = "ORDER_DATE"
     type     = "DATE"
     nullable = false
-    comment  = "Date when the order was placed."
   }
 
   column {
     name     = "TOTAL_AMOUNT"
-    type     = "DECIMAL(10,2)"
+    type     = "NUMBER(10,2)"
     nullable = false
-    comment  = "Total amount for the order."
   }
 
   column {
     name     = "STATUS"
     type     = "VARCHAR(50)"
     nullable = false
-    comment  = "Current status of the order (e.g., Pending, Shipped, Delivered)."
   }
 }
