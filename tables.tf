d722480e2b1ca17a4d33da7039a647cbb91b9bcb
Jean Rafael Sahlberg <jrsahlberg@gmail.com>
2023-08-07 23:32:06 -0300

    add orders table

--- tables.tf
+++ tables.tf
@@ -40,3 +40,47 @@ resource "snowflake_table" "user_profiles" {
     comment  = "The date when the user joined."
   }
 }
+
+
+resource "snowflake_table" "orders" {
+  provider = snowflake.sys_admin
+  database = snowflake_database.db.name
+  schema   = snowflake_schema.test_schema.name
+  name     = "ORDERS"
+  comment  = "Table to store orders placed by users."
+
+  column {
+    name     = "ORDER_ID"
+    type     = "INTEGER"
+    nullable = false
+    comment  = "Unique identifier for an order."
+  }
+
+  column {
+    name     = "USER_ID"
+    type     = "INTEGER"
+    nullable = false
+    comment  = "Foreign key referencing USER_PROFILES table."
+  }
+
+  column {
+    name     = "ORDER_DATE"
+    type     = "DATE"
+    nullable = false
+    comment  = "Date when the order was placed."
+  }
+
+  column {
+    name     = "TOTAL_AMOUNT"
+    type     = "DECIMAL(10,2)"
+    nullable = false
+    comment  = "Total amount for the order."
+  }
+
+  column {
+    name     = "STATUS"
+    type     = "VARCHAR(50)"
+    nullable = false
+    comment  = "Current status of the order (e.g., Pending, Shipped, Delivered)."
+  }
+}
