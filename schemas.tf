12ad4c947408b6b117d2f9aefc398caabaaed1bd
Jean Rafael Sahlberg <jrsahlberg@gmail.com>
2023-08-07 23:20:31 -0300

    use default provider

--- schemas.tf
+++ schemas.tf
@@ -1,5 +1,4 @@
 resource "snowflake_schema" "test_schema" {
-  provider            = snowflake.sys_admin
   database            = snowflake_database.db.name
   name                = "TEST_SCHEMA"
 }
